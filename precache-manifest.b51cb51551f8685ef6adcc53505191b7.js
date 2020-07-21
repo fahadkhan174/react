fda1de96ea742f600af3d9c95f2d02b452c27cfe
fahadkhan174 <fahadkhan174@gmail.com>
2020-07-21 21:59:42 +0530

    Updates

---
+++ precache-manifest.b51cb51551f8685ef6adcc53505191b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4fc0202afb87908e916ad86bf0371c",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "c073b251467198c25c6c",
+    "url": "/react/static/css/main.392e3a35.chunk.css"
+  },
+  {
+    "revision": "f8fe1b1dbd359063a855",
+    "url": "/react/static/js/2.9a493caa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.9a493caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c073b251467198c25c6c",
+    "url": "/react/static/js/main.36b16f5f.chunk.js"
+  },
+  {
+    "revision": "6bd774e92cfd5f0aa0e7",
+    "url": "/react/static/js/runtime-main.34445acc.js"
+  }
+]);
